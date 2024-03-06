8fb1ba36c1d4729acd3fd2743fd2da7fba8ad03a
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-03-06 19:41:28 +0200

    upd

--- js/task-1.js
+++ js/task-1.js
@@ -4,9 +4,11 @@ console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript
 console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
 
 function slugify(title) {
+  // Option 1
   // const result = title.split(' ').join('-').toLowerCase();
   // return result;
 
+  // Option 2
   const splitTitle = title.split(' ');
   const joinTitle = splitTitle.join('-');
   const transformTitleToLowerCase = joinTitle.toLowerCase();
